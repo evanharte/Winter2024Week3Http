f52e7541d8fcce546e94a840d7c151dbc89922e3
Evan <evan.harte@keyin.com>
2024-01-18 10:58:26 -0330

    added set cookie

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ const server = http.createServer((request, response) => {
       fetchFile(path);
       break;
     case "/home":
-      if (DEBUG) console.log("Home Route");
-      path += "home.html";
-      if (DEBUG) console.log("Path:", path);
-      fetchFile(path);
+      response.statusCode = 301; // moved permenently
+      response.setHeader("Location", "/");
+      response.end();
+      break;
+    case "/cookie":
+      response.setHeader("Set-Cookie", "fullName=Fred Flintstone");
+      response.end("Cookie Set");
       break;
     case "/about":
       if (DEBUG) console.log("About Route");
