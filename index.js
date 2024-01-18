e9a8936231e4b05c463afd3b60e8b3deb49cb2d6
Evan <evan.harte@keyin.com>
2024-01-18 10:08:48 -0330

    html pages & fetchFile function for routes

--- index.js
+++ index.js
@@ -4,25 +4,38 @@ global.DEBUG = true;
 
 const server = http.createServer((request, response) => {
   if (DEBUG) console.log("Request:", request.url);
+  let filename = "index.html";
   switch (request.url) {
     case "/":
       if (DEBUG) console.log("Root Route");
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.end("<h1>Welcome to the Home Page</h1>");
+      filename = "index.html";
+      fetchFile(filename);
       break;
     case "/home":
       if (DEBUG) console.log("Home Route");
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.end("<h1>Welcome to the Home Page</h1>");
+      filename = "home.html";
+      fetchFile(filename);
       break;
     case "/about":
       if (DEBUG) console.log("About Route");
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.end("<h1>Welcome to the About Page</h1>");
+      filename = "about.html";
+      fetchFile(filename);
       break;
     default:
       if (DEBUG) response.writeHead(200, { "Content-Type": "text/html" });
       response.end("404 not found");
+      break;
+  }
+  function fetchFile(fileName) {
+    fs.readFile(fileName, (error, content) => {
+      if (error) {
+        response.writeHead(500, { "Content-Type": "text/plain" });
+        response.end("500 Internal Server Error");
+      } else {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end(content, "utf-8");
+      }
+    });
   }
 });
 
